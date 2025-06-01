5af0c2feb5e41baf0577f80fb180063c1ae18ed0
DawitTenagne <dawit0038@gmail.com>
2025-06-01 01:10:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* working code !!! but not efficient = my own attempt
+
 var dice1 = Math.random() * 6;
 dice1 = Math.floor(dice1) + 1;
 var dice2 = Math.random() * 6;
@@ -37,4 +39,31 @@ if (dice1 > dice2) {
   document.querySelector("h1").innerHTML = "🚩Player Two Wins";
 } else if (dice1 === dice2) {
   document.querySelector("h1").innerHTML = "Draw!!!";
+}
+
+*/
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+//If player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
 }
